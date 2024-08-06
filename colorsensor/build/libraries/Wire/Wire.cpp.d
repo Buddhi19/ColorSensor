6041e4dfee372fd38a14dbad212b22d6ba40149a
Buddhi Wijenayake <119914594+Buddhi19@users.noreply.github.com>
2024-08-06 15:50:32 +0530

    Delete colorsensor/build directory

--- colorsensor/build/libraries/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\msi\Desktop\ColorSensor\colorsensor\build\libraries\Wire\Wire.cpp.o: \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.cpp \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility/twi.h \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.h \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
- C:\Users\msi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h
